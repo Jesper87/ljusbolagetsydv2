350f50ac717f786c669ed5630adc39e77f0503b3
Jesper <jesperdahlberg400@hotmail.com>
2016-09-26 23:24:28 +0200

    CSS

---
+++ src/LjusbolagetSyd.Core/Services/ImageService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LjusbolagetSyd.Core.Models;
+using LjusbolagetSyd.Core.Services.Interfaces;
+
+namespace LjusbolagetSyd.Core.Services
+{
+	public class ImageService : IImageService
+	{
+		public IEnumerable<GalleryImageDto> GetImagesFromContentFolder(string path)
+		{
+			var images = new List<GalleryImageDto>();
+
+			if (!Directory.Exists(path)) return images;
+
+			var fileEntries = Directory.GetFiles(path);
+			images.AddRange(fileEntries.Select(fileEntry => new GalleryImageDto {ImageUrl = fileEntry}));
+
+			return images;
+		}
+	}
+}
